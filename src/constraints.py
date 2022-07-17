05e089580ae04eb43f3bbbbb52af703a4c110065
jackdegen <jdeegs99@gmail.com>
2022-07-17 02:24:37 -0400

    finished general architechture

--- src/constraints.py
+++ src/constraints.py
@@ -1,7 +1,7 @@
 import constants
 
 slices=2
-min_salary=8500
+min_salary=7500
 cost_range = range(595,601)
 
 sg_cols = [ constants.focus_stat, constants.focus_stat_2, constants.focus_stat_3 ]
@@ -15,9 +15,10 @@ elif constants.focus_stat_3 is None:
     sg_cols = sg_cols[:2]
     val_cols = val_cols[:2]
 
-cols_to_sum = ['salary'] + sg_cols + val_cols
+cols_to_sum = ['salary', 'proj-pts'] + sg_cols + val_cols
 cols_order = [
     'salary',
+    'proj-pts',
     constants.focus_stat,
     f'{constants.focus_stat}-per-10k',
     constants.focus_stat_2,
--- src/datagolf_api.py
+++ src/datagolf_api.py
@@ -25,3 +25,16 @@ made_cut['name'] = made_cut['player_name'].apply( clean_name )
 
 def players_who_made_cut():
     return( tuple(made_cut['name'].values.tolist()) )
+
+proj_url='https://feeds.datagolf.com/preds/fantasy-projection-defaults?tour=pga&site=draftkings&slate=showdown&file_format=json&key=6a626b6c312c0d33cfe157d614b5'
+pres = requests.get(proj_url).json()
+
+proj=pd.DataFrame(pres['projections'])
+proj['name'] = proj['player_name'].apply(clean_name)
+proj=proj.loc[:,['name','proj_points']]
+proj.index=proj['name']
+proj=proj.drop('name', axis=1)
+proj.to_pickle('../data/pickle-buffer/proj-pts.pkl')
+
+def proj_pts(name):
+    return(proj.loc[name,'proj_points'] if name in proj.index else 0.0)
