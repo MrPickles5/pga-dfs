380d4be9a1bdc51f0602b686fea9ffe8a2d9d6cf
jackdegen <jdeegs99@gmail.com>
2022-07-23 15:03:02 -0400

    fixed fileman

--- src/fileman/core.py
+++ src/fileman/core.py
@@ -6,22 +6,18 @@ import constants
 class fileman:
 #     Class attributes
     
-    root = os.getcwd().replace('src','data/') #+'pickle-buffer/'
-    
-    csv_fs = root+'contest-files/'
-    pkl_fs = root+'pickle-buffer/'
+    root = os.getcwd().replace('src','data/')+'pickle-buffer/'
     
     shorten = {'tee': 'ott', 'approach': 'app', 'around': 'arg', 'green': 'putt', 'tee-to-green': 'ttg'}
-    paths = {
-        
-        'fanduel': {
-            'csv': f'{constants.tournament}.csv',
-            'pkl': 'fanduel-data.pkl'
-        },
+    fnames = {
+        'combined': 'combined.pkl',
+        'created': f'{constants.tournament}.pkl',
+        'fanduel': 'fanduel-data.pkl',
+        'skills': 'skills-decomp.pkl',
         
         'strokes' : {
-            'component': 'sg-{}.pkl', #.format()
-            'pkl': 'strokes-gained.pkl'
+            'component': 'sg-{}.pkl',
+            'aggregate': 'strokes-gained.pkl'
         },
         
         'optimizer': {
@@ -30,32 +26,45 @@ class fileman:
         }
     }
     
+    # paths = { k: root+v for k,v in files.items() }
     @classmethod
-    def fanduel(cls, param=None):
-        return cls.pkl_fs + cls.paths['fanduel']['pkl'] if param is None else cls.csv_fs + cls.paths['fanduel']['csv']
+    def combined(cls):
+        return cls.root + cls['combined']
+    
+    @classmethod
+    def created(cls):
+        return cls.root.replace('pickle-buffer', 'lineups-cretaed')+cls.fnames['created']
+    
+    @classmethod
+    def fanduel(cls):
+        return cls.root + cls.fnames['fanduel']
+    
+    @classmethod
+    def skills(cls):
+        return cls.root + cls.fnames['skills']
     
     @classmethod
     def strokes_gained(cls, param=None):
-        return cls.pkl_fs + cls.paths['strokes']['pkl'] if param is None else cls.pkl_fs + cls.paths['strokes']['component'].format(cls.shorten.get(param,param))
+        return cls.root + cls.fnames['strokes']['aggregate'] if param is None else cls.root + cls.fnames['strokes']['component'].format(cls.shorten.get(param,param))
     
     @classmethod
     def optimizer(cls, param=None):
-        return cls.pkl_fs + cls.paths['optimizer'][f'{"clean" if param is None else "raw"}']
+        return cls.root + cls.fnames['optimizer'][f'{"clean" if param is None else "raw"}']
     
     @classmethod
     def getfs(cls, ftype, fparam):
         
-        if ftype == 'created':
-            return cls.root + f'lineups-created/{constants.tournament}.pkl'
-        
-        elif ftype == 'skills':
-            return cls.pkl_fs + 'skills-decomp.pkl'
-        
-        elif ftype == 'combined':
-            return cls.pkl_fs + 'combined.pkl'
-        
-        elif ftype == 'strokes' and fparam is not None:
+        if ftype == 'strokes' and fparam is not None:
             return cls.strokes_gained(fparam)
         
-        return {'fanduel': cls.fanduel(fparam), 'strokes': cls.strokes_gained(fparam), 'optimizer': cls.strokes_gained(fparam)}.get(ftype, None)
+        ret_fs = {
+            'combined': cls.combined(),
+            'created': cls.created(),
+            'fanduel': cls.fanduel(),
+            'skills': cls.skills(),
+            'strokes': cls.strokes_gained(fparam),
+            'optimizer': cls.optimizer(fparam)
+        }
+        
+        return ret_fs.get(ftype, None)
     
